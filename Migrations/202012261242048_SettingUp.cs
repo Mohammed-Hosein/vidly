f5c462ee90fc126f0bce885939ed21c3d09d8b97
mohammed hosein <mohammed-norge@hotmail.com>
2020-12-26 16:57:28 +0100

    SettingUp Project

---
+++ Migrations/202012261242048_SettingUp.cs
@@ -0,0 +1,29 @@
+﻿namespace vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SettingUp : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate,Name) VALUES (1, 0, 0, 0,'Pay as You Go')");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate,Name) VALUES (2, 30, 1, 10,'Monthly')");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate,Name) VALUES (3, 90, 3, 15,'Quarterly')");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate,Name) VALUES (4, 300, 12, 20,'Annual')");
+            Sql("UPDATE MembershipTypes SET  Name = 'Pay as You Go' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET Name = 'Quarterly' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET Name = 'Annual' WHERE Id = 4");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Action')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Thriller')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Family')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Romance')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (5, 'Comedy')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
